48c42ba56c62779cfb1213fb1c83443df6d0f75a
meder <michael.meder@tu-berlin.de>
2017-02-14 11:10:35 +0100

    - calc session duration and number of actions - refactoring

--- data.py
+++ data.py
@@ -5,30 +5,53 @@ from datetime import datetime
 logging.basicConfig(filename='extract.log', level=logging.INFO)
 
 
-def days_between(d1, d2):
+def seconds_between(d1, d2):
     d1 = datetime.strptime(d1, "%Y-%m-%d %H:%M:%S")
     d2 = datetime.strptime(d2, "%Y-%m-%d %H:%M:%S")
     return abs((d2 - d1).seconds)
 
 
-with open('actions_1_von_4.csv', 'r', newline='') as csvfile:
+with open('data/actions_1_von_4.csv', 'r', newline='') as csvfile:
     dataReader = csv.reader(csvfile, delimiter='\t', quotechar='"')
+    headers = next(dataReader, None)  # skip the headers
+    print("""\
+    Header
+    %s
+    """ % (headers))
+    # sort data by userid and timestamp
     sortedlist = sorted(dataReader, key=lambda row: (row[0], row[1]), reverse=True)
-    i = 0
+    allActionsCount = 0
+    sessionActionsCount = 0
+    sessionActions = []
+    sessionDuration = 0
+    sessionDurationMaxDistance = 300  # in seconds
     previousRow = None
     previousUserId = None
+
+    # find periods of actions per user and calculate the usage duration and amount of actions
     for row in sortedlist:
-        print(', '.join(row))
+        if sessionActionsCount > 0:
+            actionsSecondsBetween = seconds_between(row[1], previousRow[1])
+            if row[0] != previousUserId or actionsSecondsBetween > sessionDurationMaxDistance:
+                # user id changed or max distance reached
+                print("""\
+                ********** %s : %s : %s : %s ********
+                """ % (previousUserId, sessionDuration, sessionActionsCount, sessionActions))
 
+                # reset numbers and actions list
+                sessionDuration = 0
+                sessionActionsCount = 0
+                sessionActions = []
 
-        if i > 1:
-            print(days_between(row[1], previousRow[1]))
-            if row[0] != previousUserId:
-                print(row[0])
-        else:
-            print(row)
-        if i > 30:
+            else:
+                # add time difference between those two actions
+                sessionDuration += actionsSecondsBetween
+                # add current action to the list of actions
+                sessionActions.append(row[3])
+        if allActionsCount > 300:
             break
         previousUserId = row[0]
         previousRow = row
-        i += 1
+        sessionActionsCount += 1
+        allActionsCount += 1
+        print(', '.join(row))
